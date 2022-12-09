f479cd64609262f8f8afe4dd434185967c71b5f8
ihong <ihong@uttec.co.kr>
2022-12-09 15:37:40 +0900

    model modify

--- pybo/models.py
+++ pybo/models.py
@@ -4,6 +4,9 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
     create_date = db.Column(db.DateTime(), nullable=False)
 
 class Answer(db.Model):
@@ -12,6 +15,9 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set'))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('answer_set'))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
